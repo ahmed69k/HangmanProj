5f2d684d621f729f2c1c0ec16cd0c3b31fa5f499
ahmed69k <ahmedassem04@gmail.com>
2025-10-11 15:36:13 +0300

    Project initialized

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<container></container>
+</template>
+<script>
+import container from './components/container.vue';
+export default {
+  components:{
+    container
+  }
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/store/index.js
@@ -0,0 +1,22 @@
+import {createStore} from 'vuex'
+import products from './modules/products'
+
+const store = createStore({
+    state(){
+
+    },
+    getters:{
+
+    },
+    mutations:{
+
+    },
+    actions:{
+
+    },
+    modules:{
+        products
+    }
+})
+
+export default store
